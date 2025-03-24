7e0ab9c46398a86576e61ba6209be087918ceb23
Dylan Zhang <z5478419@ad.unsw.edu.au>
2025-03-24 17:51:29 +1100

    setup socket.io

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    socket.on('chat', (msg) => {
+        console.log('message:' + msg);
+    });
+});
+
+
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
+});
