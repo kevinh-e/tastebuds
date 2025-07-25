cb5afa937189f6894a3d1e7ca10919b9349bb0f7
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2025-07-25 13:53:58 +1000

    Add msclarity

--- tastebuds/src/app/layout.js
+++ tastebuds/src/app/layout.js
@@ -23,6 +23,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "sk8pvria56");
+            `,
+          }}
+        />
+      </head>
       <AppProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
